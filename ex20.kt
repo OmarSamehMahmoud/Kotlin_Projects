38566d1a19f7073e04ebeb39178ee8350339103d
OmarSamehMahmoud <omarsamehmahmoud@gmail.com>
2025-06-22 00:16:32 +0300

    Arthimetic Operations

---
+++ ex20.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>){
+  println("Enter Num 1: ")
+  var x:Int=readLine()!!.toInt()
+  println("Enter Num 2: ")
+  var y:Int=readLine()!!.toInt()
+  println("Sum = "+(x+y))
+  println("Sub = "+(x-y))
+  println("Mul = "+(x*y))
+  println("Div = "+(x/y))
+  println("Rem = "+(x%y))
+}
