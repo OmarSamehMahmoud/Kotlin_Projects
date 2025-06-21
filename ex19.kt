6980c333e13a017abe0c606c4516baef6a41710f
OmarSamehMahmoud <omarsamehmahmoud@gmail.com>
2025-06-21 23:59:50 +0300

    When Ex 2

---
+++ ex19.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>){
+  println("Enter Name:")
+  var x:Int=readLine()!!
+  when(x){
+    "Omar" -> {
+      println("Omar")
+    }
+    "Hassan" ->{
+      println("Hassan")
+    }
+  }
+}
