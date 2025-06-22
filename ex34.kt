78545c340956fbca30cfb82ddf9064bb4f921bc7
OmarSamehMahmoud <omarsamehmahmoud@gmail.com>
2025-06-22 23:59:02 +0300

    Array Form ex1

---
+++ ex34.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>){
+  var a = arrayOf("A",2,3)
+  for ( i in 0 until a.size){
+    println("Enter Data")
+    a[i]=readLine()!!
+  }
+  for ( i in 0 until a.size){
+    println("a[$i]= "+a[i])
+  }
+}
