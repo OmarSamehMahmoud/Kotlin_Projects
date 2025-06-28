24d5de0ad041db12c293f9658661517b962da6d7
OmarSamehMahmoud <omarsamehmahmoud@gmail.com>
2025-06-28 21:09:48 +0300

    constructor ex2 init

---
+++ class6.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    val h1 = Human()
+    val h2 = Human()
+}
+
+class Human {
+    var eye: String? = null
+    var age: Int? = null
+
+    init{
+		println("Hello from constructor")
+	}
+}
